9e76b4c9c940e6259ab8c5894e344f2fb28edad5
WarBoy!4829 <149882680+GolfBallz77@users.noreply.github.com>
2026-05-12 17:16:49 -0500

    Update password and redirect page in scriptLogin.js

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,10 +1,10 @@
 function checkPassword() {
-    const correctPassword = "BillyJeans1984"; // Replace with your desired password
+    const correctPassword = "TheGolfClubIsForTheElites"; // Replace with your desired password
     const passwordInput = document.getElementById("passwordInput").value;
     const errorMessage = document.getElementById("errorMessage");
 
     if (passwordInput === correctPassword) {
-        window.location.href = "billyjeans.html"; // Redirect to your main page
+        window.location.href = "WIPIndex.html"; // Redirect to your main page
     } else {
         errorMessage.textContent = "Incorrect!";
     }
